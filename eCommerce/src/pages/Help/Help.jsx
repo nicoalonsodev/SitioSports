d081c7163054bde88100cfe74ed807c2bbea14ee
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-05-28 15:07:30 -0300

    mobile help section fixed

--- eCommerce/src/pages/Help/Help.jsx
+++ eCommerce/src/pages/Help/Help.jsx
@@ -9,55 +9,55 @@ import {
 import Contact from "../Contact/Contact";
 const Help = () => {
   return (
-    <div className="flex flex-wrap justify-center px-32 py-12">
-      <div className="flex flex-col justify-center px-32 w-full">
-        <div className="flex justify-center gap-x-6">
-          <a href="/envios" className="flex flex-col justify-center items-start bg-[#fc148c] rounded-lg hover:scale-110 duration-300">
+    <div className="flex flex-wrap justify-center px-2 lg:px-32 py-12">
+      <div className="flex flex-col justify-center px-0 lg:px-32 w-full">
+        <div className="flex flex-wrap justify-center gap-x-6 gap-y-4">
+          <a href="/envios" className="w-1/3 lg:w-1/4 flex flex-col justify-center items-start bg-[#fc148c] rounded-lg hover:scale-110 duration-300">
             <div>
               <img src={shipping_order} />
             </div>
             <div className="w-full flex justify-center">
-              <h1 className="text-center text-2xl font-semibold text-white">
+              <h1 className="text-center text-lg lg:text-2xl font-semibold text-white">
                 Envios
               </h1>
             </div>
           </a>
-          <a href="/metodos-de-pago" className="flex flex-col justify-center items-start bg-[#fc148c] rounded-lg hover:scale-110 duration-300">
+          <a href="/metodos-de-pago" className="w-1/3 lg:w-1/4 flex flex-col justify-center items-start bg-[#fc148c] rounded-lg hover:scale-110 duration-300">
             <div>
               <img src={credit_card} />
             </div>
             <div className="w-full flex justify-center">
-              <h1 className="text-center text-2xl font-semibold text-white">
+              <h1 className="text-center text-lg lg:text-2xl font-semibold text-white">
                 Pagos
               </h1>
             </div>
           </a>
-          <a href="/seguimiento-de-ordenes" className="flex flex-col justify-center items-start bg-[#fc148c] rounded-lg hover:scale-110 duration-300">
+          <a href="/seguimiento-de-ordenes" className="w-1/3 lg:w-1/4 flex flex-col justify-center items-start bg-[#fc148c] rounded-lg hover:scale-110 duration-300">
             <div>
               <img src={follow_order} />
             </div>
             <div className="w-full flex justify-center">
-              <h1 className="text-center text-2xl font-semibold text-white">
+              <h1 className="text-center text-lg lg:text-2xl font-semibold text-white">
                 Seguimiento
               </h1>
             </div>
           </a>
-          <a href="/devoluciones" className="flex flex-col justify-center items-start bg-[#fc148c] rounded-lg hover:scale-110 duration-300">
+          <a href="/devoluciones" className="w-1/3 lg:w-1/4 flex flex-col justify-center items-start bg-[#fc148c] rounded-lg hover:scale-110 duration-300">
             <div>
               <img src={returning} />
             </div>
             <div className="w-full flex justify-center">
-              <h1 className="text-center text-2xl font-semibold text-white">
+              <h1 className="text-center text-lg lg:text-2xl font-semibold text-white">
                 Devoluciones
               </h1>
             </div>
           </a>
-          <a href="/guia-de-talles" className="flex flex-col justify-center items-start bg-[#fc148c] rounded-lg p-3 hover:scale-110 duration-300">
+          <a href="/guia-de-talles" className="w-1/3 lg:w-1/4 flex flex-col justify-center items-start bg-[#fc148c] rounded-lg p-3 hover:scale-110 duration-300">
             <div className="">
               <img src={sizes_guia} />
             </div>
             <div className="w-full flex justify-center">
-              <h1 className="text-center text-2xl font-semibold text-white">
+              <h1 className="text-center text-lg lg:text-2xl font-semibold text-white">
                 Talles
               </h1>
             </div>
