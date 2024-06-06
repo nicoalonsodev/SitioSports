29d94a0f975ab092235374e7e45fffd5bf43ccc8
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-06-06 07:34:18 -0300

    changes

--- eCommerce/src/pages/ProductForm/ProductForm.jsx
+++ eCommerce/src/pages/ProductForm/ProductForm.jsx
@@ -18,7 +18,6 @@ const ProductForm = () => {
     sizes: [],
     variants: [{ variant: "", id: 1, sizes: [], imgUrl: [] }],
     color: "",
-    image: "",
     description: "",
     sub_cat: "",
   });
@@ -228,7 +227,7 @@ const ProductForm = () => {
           brand: "",
           cat: "",
           color: "",
-          image: "",
+       
           description: "",
         });
         window.location.href = "https://www.sitiosports.com/producttable";
@@ -423,30 +422,6 @@ const ProductForm = () => {
               ))}
             </div>
 
-            <div class="sm:col-span-6">
-              <label
-                for="image"
-                class="block text-sm font-medium leading-6 text-gray-900"
-              >
-                image
-              </label>
-              <div class="mt-2">
-                <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  {/* <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span> */}
-                  <input
-                    type="text"
-                    name="image"
-                    id="image"
-                    onChange={handleChange}
-                    value={form.image}
-                    autocomplete="image"
-                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                    placeholder="Nike Ultimate"
-                  />
-                </div>
-              </div>
-            </div>
-
             <div class="col-span-full">
               <label
                 for="description"
@@ -468,7 +443,7 @@ const ProductForm = () => {
                 Descripción sobre el producto.
               </p>
             </div>
-            <UploadImage handleUploadImage={handleUploadImage} />
+            {/* <UploadImage handleUploadImage={handleUploadImage} /> */}
           </div>
         </div>
       </div>
