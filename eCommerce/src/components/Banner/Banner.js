949490e7c457eac6ada53c02688cfd364fa76786
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-07-10 12:06:33 -0300

    change discount

--- eCommerce/src/components/Banner/Banner.js
+++ eCommerce/src/components/Banner/Banner.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Slider from "react-slick";
 import CustomSlide from "./CustomSlide";
-
+import { banner1, banner2, banner2_mobile } from "../../assets/images";
 const Banner = () => {
   const [dotActive, setDotActive] = useState(0);
 
@@ -14,107 +14,12 @@ const Banner = () => {
     slidesToScroll: 1,
     adaptiveHeight: true,
     arrows: false,
-    // beforeChange: (prev, next) => {
-    //   setDotActive(next);
-    // },
-    // responsive: [
-    //   {
-    //     breakpoint: 1024, // Pantallas grandes
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //       infinite: true,
-    //       dots: true,
-    //       customPaging: (i) => (
-    //         <div
-    //           style={
-    //             i === dotActive
-    //               ? {
-    //                   width: "30px",
-    //                   height: "45px",
-    //                   color: "#262626",
-    //                   borderRight: "3px #262626 solid",
-    //                   padding: "8px 0",
-    //                   cursor: "pointer",
-    //                 }
-    //               : {
-    //                   width: "30px",
-    //                   height: "45px",
-    //                   color: "transparent",
-    //                   borderRight: "3px white solid",
-    //                   padding: "8px 0",
-    //                   cursor: "pointer",
-    //                 }
-    //           }
-    //         >
-    //           {/* 0{i + 1} */}
-    //         </div>
-    //       ),
-    //       appendDots: (dots) => (
-    //         <div
-    //           style={{
-    //             position: "absolute",
-    //             top: "50%",
-    //             left: "7%",
-    //             transform: "translateY(-50%)",
-    //           }}
-    //         >
-    //           <ul style={{ margin: "0px" }}> {dots} </ul>
-    //         </div>
-    //       ),
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 576, // Pantallas pequeñas
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //       infinite: true,
-    //       dots: true,
-    //       customPaging: (i) => (
-    //         <div
-    //           style={
-    //             i === dotActive
-    //               ? {
-    //                   width: "25px",
-    //                   color: "#262626",
-    //                   borderRight: "3px #262626 solid",
-    //                   cursor: "pointer",
-    //                   fontSize: "12px",
-    //                 }
-    //               : {
-    //                   width: "25px",
-    //                   color: "transparent",
-    //                   borderRight: "3px white solid",
-    //                   cursor: "pointer",
-    //                   fontSize: "12px",
-    //                 }
-    //           }
-    //         >
-    //           0{i + 1}
-    //         </div>
-    //       ),
-    //       appendDots: (dots) => (
-    //         <div
-    //           style={{
-    //             position: "absolute",
-    //             top: "50%",
-    //             left: "2%",
-    //             transform: "translateY(-50%)",
-    //           }}
-    //         >
-    //           <ul style={{ margin: "0px" }}> {dots} </ul>
-    //         </div>
-    //       ),
-    //     },
-    //   },
-    // ],
   };
 
   const slides = [
     {
       imgSrc:
-        "https://res.cloudinary.com/dtf3dfpnw/image/upload/v1717595311/NUESTRA_CAMISETA_1_efphhh.jpg",
+        banner1,
       text: "Quality Printing Solutions",
       Subtext:
         "Discover our wide range of printers and consumables designed for professional printing needs.",
@@ -122,7 +27,7 @@ const Banner = () => {
       buttonText: "Comprar Ahora",
     },
     {
-      imgSrc: "https://res.cloudinary.com/dtf3dfpnw/image/upload/v1717595294/Dise%C3%B1o_sin_t%C3%ADtulo_39_ejjava.png",
+      imgSrc: banner2,
       text: "Enhance Your Printing Experience",
       Subtext:
         "Explore our premium printers and consumables for exceptional results",
@@ -130,7 +35,7 @@ const Banner = () => {
       buttonText: "Comprar Ahora",
     },
     {
-      imgSrc: "https://res.cloudinary.com/dtf3dfpnw/image/upload/v1718304229/Dise%C3%B1o_sin_t%C3%ADtulo_46_rwettj.png",
+      imgSrc: banner2_mobile,
       text: "Efficiency Redefined",
       Subtext:
         "Maximize productivity with our advanced printers and high-quality consumables. ",
--- eCommerce/src/components/home/Products/Product.js
+++ eCommerce/src/components/home/Products/Product.js
@@ -31,16 +31,20 @@ const Product = (props) => {
 
   const discountedPrice = calculateDiscountedPrice();
 
-
   const { compare_price, price } = props;
 
   // Calcular el porcentaje de descuento
-  const discountPercentage = compare_price && parseFloat(compare_price) !== 0.00
-    ? Math.round(((parseFloat(compare_price) - parseFloat(price)) / parseFloat(compare_price)) * 100)
-    : 0;
+  const discountPercentage =
+    compare_price && parseFloat(compare_price) !== 0.0
+      ? Math.round(
+          ((parseFloat(compare_price) - parseFloat(price)) /
+            parseFloat(compare_price)) *
+            100
+        )
+      : 0;
 
   // Convertir el porcentaje de descuento a cadena si es mayor que 0
-  const discountText = discountPercentage > 0 ? `${discountPercentage}%` : null;
+  const discountText = discountPercentage > 0 ? `${discountPercentage}%` : "";
 
   return (
     <div className="w-full relative group">
@@ -49,9 +53,7 @@ const Product = (props) => {
           <Image className="w-full h-full" imgSrc={props.img} />
         </div>
         <div className="absolute top-4 left-4">
-          {discountPercentage !== 0 ? (
-            <Badge text={`${discountText}`} />
-          ) : null}
+          {discountPercentage !== 0 ? <Badge text={`${discountText}`} /> : null}
         </div>
         <div className="w-full h-auto absolute bg-white -bottom-[130px] group-hover:bottom-0 duration-700">
           <ul className="w-full h-full flex flex-col items-end justify-center gap-2 font-titleFont px-2 border-l border-r">
@@ -68,24 +70,18 @@ const Product = (props) => {
         </div>
       </div>
       <div className="max-w-80 py-2 flex flex-col gap-1 border-[1px] border-t-0 px-4">
-        <div className="flex items-center justify-between ">
+        <div className="flex items-center justify-between">
           <h2 className="text-sm text-gray-700">{props.productName}</h2>
         </div>
-        <div className="flex gap-x-2">
+        <div className="flex gap-x-2 items-center">
+          <p className="text-[#767676] font-bold text-[14px]">
+            ${formatPrice(props.price)}
+          </p>
           {props.compare_price && parseFloat(props.compare_price) !== 0.0 ? (
-            <p className="text-[#767676] font-bold text-[14px]">
+            <p className="text-[#767676] line-through text-[14px]">
               ${formatPrice(props.compare_price)}
             </p>
           ) : null}
-          <p
-            className={`${
-              props.compare_price && parseFloat(props.compare_price) !== 0.0
-                ? "line-through"
-                : ""
-            } text-[#767676] font-semibold text-[14px]`}
-          >
-            ${formatPrice(props.price)}
-          </p>
         </div>
       </div>
     </div>
