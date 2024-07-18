419a36ae01ef52d25a9ae1c5e1445c30658b5532
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-07-18 10:41:36 -0300

    delete commission added

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,25 +6,32 @@ const { getAllProductsHandler } = require("../handlers/getAllProductsHandler");
 const { getProductByIdHandler } = require("../handlers/getProductByIdHandler");
 const { getUsersHandler } = require("../handlers/getUsersHandler");
 const { deleteProductHandler } = require("../handlers/deleteProductHandler");
-const { getAllCommissionsHandler } = require("../handlers/getAllCommissionsHandler");
-const { createOrder, receiveWebhook } = require("../controllers/paymentController");
+const {
+  getAllCommissionsHandler,
+} = require("../handlers/getAllCommissionsHandler");
+const {
+  createOrder,
+  receiveWebhook,
+} = require("../controllers/paymentController");
 const { postProductHandler } = require("../handlers/postProductHandler");
 const { postUserHandler } = require("../handlers/postUserHandler");
 const { postOrderHandler } = require("../handlers/postOrderHandler");
-const { postCommissionsHandler} = require("../handlers/postCommissionHandler")
+const { postCommissionsHandler } = require("../handlers/postCommissionHandler");
 const { putCommissionsHandler } = require("../handlers/putCommissionsHandler");
 const { putUserHandler } = require("../handlers/putUserHandler");
 const { putProductHandler } = require("../handlers/putProductHandler");
 const { putOrderHandler } = require("../handlers/putOrderHAndler");
-const { authenticateToken } = require('../helpers/authenticateToken');
-const {loginHandler} = require('../handlers/authHandler');
-const {protectedRouteHandler} = require('../handlers/authHandler');
-const {deleteUserHandler} = require('../handlers/deleteUserHandler');
+const { authenticateToken } = require("../helpers/authenticateToken");
+const { loginHandler } = require("../handlers/authHandler");
+const { protectedRouteHandler } = require("../handlers/authHandler");
+const { deleteUserHandler } = require("../handlers/deleteUserHandler");
+const {
+  deleteCommissionhandler,
+} = require("../handlers/deleteCommissionHandler");
 
 router.get("/products", getAllProductsHandler);
 router.get("/products/:slug", getProductByIdHandler);
 router.get("/users", getUsersHandler);
-// router.get("/users/:id", getUserByIdHandler);
 router.get("/orders", getOrdersHandler);
 router.get("/commissions", getAllCommissionsHandler);
 
@@ -32,15 +39,15 @@ router.get("/commissions", getAllCommissionsHandler);
 // router.get('/pending', (req, res)=> res.send("pend") );
 // router.get('/failure', (req, res)=> res.send("fail"));
 
-router.post('/login', loginHandler);
-router.get('/admin', authenticateToken, protectedRouteHandler);
+router.post("/login", loginHandler);
+router.get("/admin", authenticateToken, protectedRouteHandler);
 
 router.post("/products", postProductHandler);
 router.post("/users", postUserHandler);
 router.post("/commissions", postCommissionsHandler);
 router.post("/create-order", createOrder);
-router.post('/webhook', receiveWebhook);
-router.post('/order', postOrderHandler);
+router.post("/webhook", receiveWebhook);
+router.post("/order", postOrderHandler);
 
 router.put("/products/:id", putProductHandler);
 router.put("/users/:id", putUserHandler);
@@ -49,5 +56,6 @@ router.put("/order/:id", putOrderHandler);
 
 router.delete("/product/:id", deleteProductHandler);
 router.delete("/user/:id", deleteUserHandler);
+router.delete("/commissions/:id", deleteCommissionhandler);
 
 module.exports = router;
